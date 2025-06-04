2fb24e21d0f69d3bdb3db3ccc268e79db4eb516b
Seldon <daanmaro99june@gmail.com>
2025-06-04 16:58:47 -0600

    feat(Cotizacion): Cotización de Autos con los datos de Chevrolet, Aveo.

---
+++ tests/cotizacionAutos.spec.ts
@@ -0,0 +1,58 @@
+import { test, expect } from '@playwright/test';
+
+test('Cotización Autos: ', async ({ page }) => {
+    /* Aumentar el tiempo */
+    test.setTimeout(60000); //Valor default 30000
+
+    await page.goto('http://localhost:5173/opl/login?subRamo=autos&instancia=EPEPSICO');
+    
+    /* Inicio de Sesión */
+    await page.getByRole('textbox', { name: 'GPID' }).click();
+    await page.getByRole('textbox', { name: 'GPID' }).fill('devautos');
+    await page.getByRole('textbox', { name: 'CONTRASEÑA' }).click();
+    await page.getByRole('textbox', { name: 'CONTRASEÑA' }).fill('devPW6#7');
+    await page.getByRole('textbox', { name: 'Ingrese el texto de la imagen' }).click();
+    await page.getByRole('textbox', { name: 'Ingrese el texto de la imagen' }).fill('55555');
+    await page.getByRole('button', { name: 'Acceder' }).click();
+    /* Cotización */
+    await page.getByRole('textbox', { name: 'GPID' }).click();
+    await page.getByRole('textbox', { name: 'GPID' }).fill('ROMA821011MDFJGL01');
+    await page.getByRole('textbox', { name: 'PARENTESCO' }).click();
+    await page.getByText('EMPLEADO', { exact: true }).click();
+    await page.getByRole('textbox', { name: 'INSTITUCIÓN' }).click();
+    await page.getByText('COMERCIALIZADORA PEPSICO').click();
+    await page.getByRole('textbox', { name: 'TIPO DE VEHICULO' }).click();
+    await page.getByText('AUTOMÓVIL', { exact: true }).click();
+    await page.getByRole('combobox').filter({ hasText: 'TIPO DE UNIDAD' }).locator('i').nth(1).click();
+    await page.getByText('NUEVO', { exact: true }).click();
+    await page.getByRole('combobox').filter({ hasText: 'MODELO' }).locator('i').nth(1).click();
+    await page.getByRole('option', { name: '2024' }).locator('div').first().click();
+    await page.getByRole('combobox').filter({ hasText: 'MARCA' }).locator('i').nth(1).click();
+    
+    // Esperar que cargue el dropdown y hacer scroll hacia CHEVROLET
+    await page.waitForSelector('text=CHEVROLET', { timeout: 10000 });
+    await page.getByText('CHEVROLET').scrollIntoViewIfNeeded();
+    await page.waitForTimeout(2000);
+    await page.getByText('CHEVROLET').click();
+    
+
+    // Esperar que carguen los modelos de Chevrolet
+    await page.waitForTimeout(3000); // Espera 3 segundos
+    await page.getByRole('combobox').filter({ hasText: 'DESCRIPCIÓN' }).locator('i').nth(1).click();
+    await page.getByText('AVEO - LT PLUS F 4P L4 1.5L').scrollIntoViewIfNeeded();
+    await page.getByText('AVEO - LT PLUS F 4P L4 1.5L').click();
+        
+
+        
+        
+    await page.getByRole('combobox').filter({ hasText: 'FORMA DE PAGO' }).locator('i').nth(1).click();
+    await page.getByText('Pago Referenciado OPL').click();
+    await page.getByRole('textbox', { name: 'PAQUETE DESEADO' }).click();
+    await page.getByText('amplia DM 5').click();
+    await page.getByRole('button', { name: 'Cotizar' }).click();
+    
+    // Esperar a que se procese la cotización
+    await page.waitForTimeout(3000); // Espera 3 segundos
+    
+    await page.getByRole('button', { name: 'Continuar' }).click();
+});
