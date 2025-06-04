4bd096853b705ffc7c249d40fdaee494c90457a4
Seldon <daanmaro99june@gmail.com>
2025-06-04 14:16:50 -0600

    fix(test): Prueba de Cotización, directo, sesión iniciada.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+/* Pruba de uso de Cotización dentro de Salud. 
+    - Se ingresa a la URL de cotización.
+      Consideración de que la sesión ya está iniciada, de lo contraio sería iniciar sesión.
+      Sin embargo, existe un captcha, y habría que poner a playwright a resolverlo con AI.
+    - Se ingresa el nombre del asegurado.
+    - Se agrega el asegurado.
+    - Se realiza la cotización.
+    - Se espera que se muestre el resultado de la cotización.
+*/
+//Función de test.
+test('test', async ({ page }) => {
+  await page.goto('http://11.50.0.151:7003/opl/cotizador');
+  await page.getByRole('textbox', { name: 'Usuario' }).click();
+  await page.getByRole('textbox', { name: 'NOMBRE ASEGURADO' }).click();
+  await page.getByRole('textbox', { name: 'NOMBRE ASEGURADO' }).fill('Alda Jocelyn');
+  await page.getByRole('button', { name: 'Agregar asegurado' }).click();
+  await page.getByRole('button', { name: 'Cotizar' }).click();
 });
